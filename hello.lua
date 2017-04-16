8b9397070c38cdc98ac3d5dea884d6d46d87e956
spring <www.springworder@gmail.com>
2017-04-16 08:35:48 +0800

    LuaStudy
    about lua study test

---
+++ hello.lua
@@ -0,0 +1,6 @@
+function say(i)
+	print(i)
+end
+say("Hello World!")
+print ("springworder@gmail.com")
+print(ss)
---
+++ tabletest.lua
@@ -0,0 +1,4 @@
+table1={key1="var1",key2="var2"}
+for k,v in pairs(table1) do
+	print(k.."-"..v)
+end

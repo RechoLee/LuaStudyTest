a414b8a7127fe1aee3bc77daa7ef659c5b0d3eb7
spring <www.springworder@gmail.com>
2017-04-24 23:40:45 +0800

    loop

---
+++ loop.lua
@@ -0,0 +1,25 @@
+--loop some diffients loop type
+
+--while
+a=10
+while (a<=20)
+	do
+		print(a)
+		a=a+1
+	end
+
+--for 
+for i=10,20,1 do
+	print(i)
+end
+
+--for 2
+for i=20,10,-1 do
+	print (i)
+end
+
+table1={"nihao","zhangsan","lisi"}
+
+for k,v in pairs(table1) do
+	print (k,v)
+end
